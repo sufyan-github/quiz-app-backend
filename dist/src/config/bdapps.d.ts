6666c9c899c843708134d0cab5f25a952338932c
sufyan-github <abusufyan.cse20@gmail.com>
2026-07-28 01:27:56 +0600

    Manual Deply Part1

--- dist/src/config/bdapps.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const bdappsConfig: {
-    appId: string;
-    password: string;
-    appHash: string;
-};
-//# sourceMappingURL=bdapps.d.ts.map
--- dist/src/config/bdapps.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bdappsConfig = void 0;
-exports.bdappsConfig = {
-    appId: process.env.BDAPPS_APP_ID || process.env.APP_ID || '',
-    password: process.env.BDAPPS_PASSWORD || process.env.PASSWORD || '',
-    appHash: process.env.BDAPPS_APP_HASH || process.env.APP_HASH || ''
-};
-//# sourceMappingURL=bdapps.js.map
